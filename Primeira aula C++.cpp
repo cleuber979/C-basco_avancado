cfbe50f4fa669a29762037bf09ca597040196685
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-06 21:56:24 -0300

    Add files via upload

---
+++ Primeira aula C++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Vou Aprender a programar na Linguagem C++"<<std::endl;
+    
+    system("Pause ");
+    return 0;
+}
