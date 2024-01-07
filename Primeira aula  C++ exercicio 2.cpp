e94bdf1b6286bf16f67e7bd93177bb8119212b74
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-06 23:13:08 -0300

    Add files via upload

---
+++ Primeira aula  C++ exercicio 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Vou" <<std::endl;
+	std::cout<<"Aprender"<<std::endl;
+	std::cout<<"a"<<std::endl; 
+	std::cout<<"programar"<<std::endl;
+	std::cout<<"na"<<std::endl;
+	std::cout<<"Linguagem"<<std::endl;
+	std::cout<<"C++"<<std::endl;
+    
+    system("Pause ");
+    return 0;
+}
