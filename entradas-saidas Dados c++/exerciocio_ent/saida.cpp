8185430d8ec200c129e97f0772a11fbf60d335ec
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-05-11 15:12:07 -0300

    Add files via upload

---
+++ entradas-saidas Dados c++/exerciocio_ent/saida.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    
+    int number,number2,soma;
+    std::cout<<"Por favor digite um numero inteiro"<<endl;
+    cin>>number;
+     std::cout<<"Por favor digite segundo numero inteiro"<<endl;
+     cin>>number2;
+     soma = number + number2;
+     std::cout<<"O soma dos numeros e: "<<soma<<std::endl;
+
+    system("PAUSE");
+
+};
